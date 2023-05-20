d17c4ee93161b9e480d7d4a3611eb58a6d79dbeb
stFamilra <stfamilra@mail.ru>
2023-05-20 21:23:54 +0300

    feat: add rules bottom

--- pigGame/script.js
+++ pigGame/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModalWindow = document.querySelector('.close-modal-window');
+const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
+
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.querySelector('#score--1');
 const current0Element = document.querySelector('#current--0');
@@ -100,5 +105,29 @@ btnNew.addEventListener('click', () => {
   if (!player0.classList.contains('player--active')) {
     player0.classList.add('player--active');
     player1.classList.remove('player--active');
+  }
+});
+
+const switchModalWindow = () => {
+  modalWindow.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+};
+
+const handlerModalWindow = value => {
+  value.addEventListener('click', e => {
+    switchModalWindow();
+  });
+};
+
+for (let i = 0; i < btnsShowModalWindow.length; i++) {
+  handlerModalWindow(btnsShowModalWindow[i]);
+}
+
+handlerModalWindow(btnCloseModalWindow);
+handlerModalWindow(overlay);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modalWindow.classList.contains('hidden')) {
+    switchModalWindow();
   }
 });
