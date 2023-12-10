9da49f0de1cb487a9d4f5fca0147d573698f6143
stFamilra <betti.muchitel@mail.ru>
2023-12-10 21:03:23 +0300

    feat: add marker popap and form

--- cardioApp/script.js
+++ cardioApp/script.js
@@ -5,5 +5,75 @@ const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--temp');
-const inputElevation = document.querySelector('.form__input--climb');
+const inputTemp = document.querySelector('.form__input--temp');
+const inputClimb = document.querySelector('.form__input--climb');
+
+let map, mapEvent;
+//проверяем, есть ли у браузера возможность получить геолокацию
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // получаем широту и долготы обычным способом и через деструктуризацию
+      //   const latitude = position.coords.latitude;
+      // const longitude = position.coords.longitude
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      const coords = [latitude, longitude];
+      map = L.map('map').setView(coords, 13);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // on - это что-то вроде eventListener, установленный внутри библиотеки.
+      map.on('click', e => {
+        mapEvent = e;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+        // console.log(e);
+        // L.marker(e.latlng)
+        //   .addTo(map)
+        //   .bindPopup(
+        //     L.popup({
+        //       maxWidth: 200,
+        //       minWidth: 100,
+        //       closeOnClick: false,
+        //       autoClose: false,
+        //       className: 'running-popup',
+        //     })
+        //   );
+        //   .setPopupContent('Тренировка')
+        //   .openPopup();
+      });
+    },
+    function () {
+      alert('Невозможно получить Ваше местоположение!');
+    }
+  );
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(mapEvent);
+  // Очистка полей ввода данных
+  inputDistance.value =
+    inputDuration.value =
+    inputTemp.value =
+    inputClimb.valuue =
+      '';
+
+  // Отображение маркера
+  L.marker(mapEvent.latlng)
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 200,
+        minWidth: 100,
+        closeOnClick: false,
+        autoClose: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Тренировка')
+    .openPopup();
+});
