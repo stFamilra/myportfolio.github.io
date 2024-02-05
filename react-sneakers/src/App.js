9e1788329accc24eecb1fbad37bfe436bce985af
stFamilra <betti.muchitel@mail.ru>
2024-02-06 01:08:20 +0300

    feat

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -1,10 +1,55 @@
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
+
+const sneakerCards = [
+  {
+    name: "Мужские Кроссовки Under Armour Curry 8",
+    price: 12999,
+    src: "/img/sneakers/1.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    src: "/img/sneakers/2.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Lebron XVIII Low",
+    price: 13999,
+    src: "/img/sneakers/3.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Kyrie 7",
+    price: 11299,
+    src: "/img/sneakers/4.jpg",
+  },
+];
+
 function App() {
   return (
-    <div className="wrapper">
-      <header>
-        <div className="header__container"></div>
-      </header>
-      <main></main>
+    <div className="wrapper clear">
+      <Drawer />
+      <Header />
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block d-flex">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
+
+        <div className="d-flex flex-wrap">
+          {sneakerCards.map((card) => (
+            <Card
+              name={card.name}
+              price={card.price}
+              src={card.src}
+              onClickPlus={() => console.log(5)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ react-sneakers/src/components/Card/index.js
@@ -0,0 +1,25 @@
+import styles from "./Card.module.scss";
+console.log(styles);
+
+function Card(props) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.favorite}>
+        <img src="/img/heart-unliked.svg" alt="Unliked" />
+      </div>
+      <img width={133} height={112} src={props.src} alt="Sneakers" />
+      <h5>{props.name}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>{props.price} руб.</b>
+        </div>
+        <button className="button" onClick={props.onClickPlus}>
+          <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
